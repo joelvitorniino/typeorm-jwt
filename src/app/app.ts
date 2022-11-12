d254618465ddc4b6047a20be559c36772c1c03f4
joelvitorniino <joelprofessor13@gmail.com>
2022-11-12 11:06:39 -0300

    route of add user

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes';
+import { AppDataSource } from './db/data-source';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 app.use(router);
 
 app.listen(3000, () => {
     console.log("Running on http://localhost:3000");
-});
+})
+
+AppDataSource.initialize()
+    .then(() => console.log("Database connected!"))
+    .catch((e) => console.log("Error on connection of database!", e));
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import UserController from "./controllers/UserController";
 
 const router = Router();
+
+router.post('/api/v1/users', UserController.store);
 
 export default router;
