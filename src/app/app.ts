85bfc64f9627218dfcc59bb8a7f9fe60ccce82a4
joelvitorniino <joelprofessor13@gmail.com>
2022-11-12 10:19:38 -0300

    structure initial to express app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes';
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors);
+app.use(router);
+
+app.listen(3000, () => {
+    console.log("Running on http://localhost:3000");
+});
