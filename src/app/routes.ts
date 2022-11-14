23993c8d597f9576a70ca67df819fdf5d1d746c9
joelvitorniino <joelprofessor13@gmail.com>
2022-11-14 07:29:57 -0300

    create routes of auth and users

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import AuthController from "./controllers/AuthController";
 import UserController from "./controllers/UserController";
+import authMiddleware from "./middleware/authMiddleware";
 
 const router = Router();
 
 router.post('/api/v1/users', UserController.store);
+router.post('/api/v1/auth', AuthController.authenticate);
+router.get('/api/v1/users', authMiddleware, UserController.index);
 
 export default router;
